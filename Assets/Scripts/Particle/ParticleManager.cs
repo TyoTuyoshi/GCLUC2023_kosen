cce5528ef75f86de4b1f8d0e114d100744d855c1
eir <89068705+gongitsune@users.noreply.github.com>
2023-03-11 15:42:08 +0900

    Particle再生用のマネージャー作成

--- Assets/Scripts/Particle/ParticleManager.cs
+++ Assets/Scripts/Particle/ParticleManager.cs
@@ -1,31 +1,65 @@
+using System;
 using System.Linq;
+using AutoGenerate;
+using Cysharp.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.VFX;
 using Utils;
 
 namespace Particle
 {
     public class ParticleManager : MonoBehaviour
     {
+        private static ParticleManager _instance;
         [SerializeField] private AddressableAssetGroup vfxGroup;
+        private AssetReferenceT<VisualEffect>[] _vfxRefs;
         public AddressableAssetGroup VfxGroup => vfxGroup;
+
+        public static ParticleManager Instance => _instance ??= FindObjectOfType<ParticleManager>();
+
+        private void Awake()
+        {
+            _vfxRefs = vfxGroup.entries.Select(e => new AssetReferenceT<VisualEffect>(e.guid)).ToArray();
+        }
+
+        private void OnDestroy()
+        {
+            _instance = null;
+        }
+
+        public async UniTask PlayVfx(VfxEnum vfxType, float durationSec)
+        {
+            var assetRef = _vfxRefs[(int)vfxType];
+            var vfx = await Addressables.InstantiateAsync(assetRef);
+            await UniTask.Delay(TimeSpan.FromSeconds(durationSec));
+            assetRef.ReleaseInstance(vfx);
+        }
     }
 
 #if UNITY_EDITOR
     [CustomEditor(typeof(ParticleManager))]
     public class ParticleManagerEditor : Editor
     {
+        private (VfxEnum, float, bool) _vfx;
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
             var manager = (target as ParticleManager)!;
 
-            if (Application.isPlaying || manager.VfxGroup == null) return;
-
-            if (GUILayout.Button("Detect Vfx"))
+            if (!Application.isPlaying && manager.VfxGroup != null && GUILayout.Button("Detect Vfx"))
                 EnumCreator.CreateSource(manager.VfxGroup.entries.Select(v => v.address),
                     "Assets/Plugins/AutoGenerate/VfxEnum.cs");
+
+            if (Application.isPlaying && (_vfx.Item3 = EditorGUILayout.Foldout(_vfx.Item3, "Debug")))
+            {
+                _vfx.Item1 = (VfxEnum)EditorGUILayout.EnumPopup("Vfx", _vfx.Item1);
+                _vfx.Item2 = EditorGUILayout.FloatField("duration", _vfx.Item2);
+                if (GUILayout.Button("Play")) UniTask.Create(async () => await manager.PlayVfx(_vfx.Item1, _vfx.Item2));
+            }
         }
     }
 #endif

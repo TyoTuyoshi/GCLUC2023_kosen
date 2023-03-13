42d33c77b3337e5662f7e84cef8e671207deac9d
TyoTuyoshi <futureRPG@outlook.jp>
2023-03-13 20:57:41 +0900

    stage1のレイアウト_素材追加

--- Assets/Yuki/Script/Stage/Stage1.cs
+++ Assets/Yuki/Script/Stage/Stage1.cs
@@ -5,31 +5,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Scene;
 using System.IO;
+using TMPro;
 
 public class Stage1 : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI tmp_timer;
+
+    private float time = 0;
     //デバッグ用スクリプト
     void Start()
     {
         //ステージクリア扱い
-        {
+        /*{
             MapStateJson msjson = new MapStateJson();
             msjson.statusFlag = new bool[] { true, true, false, false };
             string jsonString = JsonUtility.ToJson(msjson);
             File.WriteAllText(Application.dataPath + "/Resources/StageStatus.json", jsonString);
-        }
-        Invoke("BackMapSelect", 2.0f);
+        }*/
+        //Invoke("BackMapSelect", 2.0f);
     }
-
+    
+    void Update()
+    {
+        time += Time.deltaTime;
+        tmp_timer.text = time.ToString("f1");
+    }
+    
     private void BackMapSelect()
     {
         //SceneLoader.Instance.TransitionScene("TitleScene");
         SceneManager.LoadScene("TitleScene");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }

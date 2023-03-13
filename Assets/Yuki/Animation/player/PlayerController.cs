911f4d499654f3761086fe8a294df72edc52b5fc
TyoTuyoshi <futureRPG@outlook.jp>
2023-03-13 10:16:38 +0900

    rootmotion途中

--- Assets/Yuki/Animation/player/PlayerController.cs
+++ Assets/Yuki/Animation/player/PlayerController.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PkayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    
+    private InputAction move, jump;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerInput playerInput = gameObject.GetComponent<PlayerInput>();
+        move = playerInput.actions["Move"];
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Vector2 vec2 = new Vector2(Keyboard.current.)
+        Vector2 m = move.ReadValue<Vector2>();
+        
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             _animator.SetTrigger("Attack");
         }
+
+        _animator.SetFloat("run", Mathf.Abs(m.magnitude));
+        
     }
 }

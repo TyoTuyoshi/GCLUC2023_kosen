1110b86d63809a551694b70d18cbc705bf412ec9
eir <89068705+gongitsune@users.noreply.github.com>
2023-03-19 09:08:50 +0900

    敵のコライダーを変更

--- Assets/Scripts/Actor/Enemy/Enemy.Damageable.cs
+++ Assets/Scripts/Actor/Enemy/Enemy.Damageable.cs
@@ -40,10 +40,13 @@ namespace Actor.Enemy
             hpBar.value = CurrentHp / MaxHp;
 
             // ノックバック
-            var dir = (_rigid.position - (Vector2)e.SourcePos).normalized * e.KnockBackPower;
+            // var dir = ((Vector2)origin.position - (Vector2)e.SourcePos).normalized * e.KnockBackPower;
+            var dir = Vector2.zero;
+            dir.x = _rigid.position.x < e.SourcePos.x ? e.KnockBackPower : -e.KnockBackPower;
+
             _rigid.AddForce(dir, ForceMode2D.Impulse);
             // 一定時間後にリセット
-            // DOVirtual.DelayedCall(1f, () => _rigid.velocity = Vector2.zero).SetLink(gameObject);
+            DOVirtual.DelayedCall(1f, () => _rigid.velocity = Vector2.zero).SetLink(gameObject);
 
             _stateMachine.SendEvent(CurrentHp <= 0 ? EnemyState.Death : EnemyState.Damage);
         }
